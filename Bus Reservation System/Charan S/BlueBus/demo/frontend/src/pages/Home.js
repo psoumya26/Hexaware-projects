e97e9ac664ddea74e2995c96ed409032e25fdd51
charan <charansiva2001@gmail.com>
2023-03-15 16:28:27 +0530

    Bus Project(Charan S, Sutharsan S)

---
+++ Bus Reservation System/Charan S/BlueBus/demo/frontend/src/pages/Home.js
@@ -0,0 +1,17 @@
+import './Home.css';
+import React from 'react';
+const Home = () => {
+
+    return (
+        <div>
+            <div className="banner-container">
+                <div className="text-center">
+                <h1 className=" text-6xl text-white font-bold font-extrabold">Welcome to BlueBus</h1>
+                <h4 className="text-4xl mt-8 text-white">Book your tickets hassle-free with us</h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
